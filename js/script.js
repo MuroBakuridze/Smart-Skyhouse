dd68a3efef41c17bc694c147c8fc818a33b7acdc
MuroBakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-07-12 18:58:41 +0400

    sec pagination

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper1", {
   slidesPerView: 3.5,
   spaceBetween: 25,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swp1",
+    clickable: true,
+  },
+});
+
+
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swp2",
     clickable: true,
   },
 });
