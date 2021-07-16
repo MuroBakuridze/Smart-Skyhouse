4b1348748ff965fca528879a2e88eff028cf534b
MuroBakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-07-16 18:38:57 +0400

    first slide pagination

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper1", {
+var swiper = new Swiper(".mySwiper2", {
   slidesPerView: 3.5,
   spaceBetween: 25,
   pagination: {
@@ -9,3 +9,17 @@ var swiper = new Swiper(".mySwiper1", {
 
 
 
+var swiper1 = new Swiper(".mySwiper1", {
+  cssMode: true,
+  spaceBetween: 25,
+  slidesPerView:2,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
