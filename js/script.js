d5b1f3db4d80c4dbf85d9a57bd4d25a7cdf5caf1
MuroBakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-07-17 22:00:35 +0400

    first slider correction

--- js/script.js
+++ js/script.js
@@ -8,18 +8,34 @@ var swiper = new Swiper(".mySwiper2", {
 });
 
 
-
-var swiper1 = new Swiper(".mySwiper1", {
-  cssMode: true,
-  spaceBetween: 25,
-  slidesPerView:2,
+var swiper1 = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 50,
+  slidesPerGroup: 1,
+  loop: false,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
 });
+
+
+// var swiper1 = new Swiper(".mySwiper1", {
+//   cssMode: true,
+//   spaceBetween: 25,
+//   slidesPerView:2,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
