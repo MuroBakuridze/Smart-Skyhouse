824e639746b0dbf0bcc5c2f2fe6cfd7b47eedb2c
MuroBakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-07-21 20:47:17 +0400

    dixed header and smooth scroll

--- js/script.js
+++ js/script.js
@@ -25,17 +25,3 @@ var swiper1 = new Swiper(".mySwiper", {
 });
 
 
-// var swiper1 = new Swiper(".mySwiper1", {
-//   cssMode: true,
-//   spaceBetween: 25,
-//   slidesPerView:2,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-//   mousewheel: true,
-//   keyboard: true,
-// });
